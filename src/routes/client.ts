53616a2bd701a1ada3cb83bc4d0210e60a9d9069
Andy <andy@hydrogenplatform.com>
2018-09-17 13:45:18 -0400

    Forgot to save

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -18,7 +18,6 @@ router.post('/signature', signatureArguments, (req: Request, res: Response, next
   const errors = validationResult(req)
   if (!errors.isEmpty()) res.status(422).json(errors.array())
 
-<<<<<<< HEAD
     withConnection(async (connection: Connection) => {
       const signatureRepository = connection.manager.getRepository(Signature)
 
@@ -35,19 +34,6 @@ router.post('/signature', signatureArguments, (req: Request, res: Response, next
       }
     }).catch((error: any) => {
         return next(error)
-=======
-  withConnection((connection: Connection) => {
-    const signatureRepository = connection.manager.getRepository(Signature)
-    const newSignature = signatureRepository.create(req.body)
-
-    return signatureRepository.save(newSignature)
-      .then((signatures: Signature[]) => {
-        res.json(signatures)
-      })
-  })
-    .catch((error: any) => {
-      next(error)
->>>>>>> 3e093ba7a4f8895dec501b902963af3826f59723
     })
 })
 
